067a818c642668b54ef256a012c4f4452709f5e4
asnashameel <2020.cse.asna@ekc.edu.in>
2025-01-24 21:59:32 +0530

    put endpoint

--- main.py
+++ main.py
@@ -27,6 +27,7 @@ class SimpleCRUDHandler(BaseHTTPRequestHandler):
         else:
             self._set_headers(404)
             self.wfile.write(json.dumps({"error": "Invalid endpoint"}).encode())
+
     def do_POST(self):
         global data_store, next_id
         if self.path == "/items":
@@ -39,6 +40,24 @@ class SimpleCRUDHandler(BaseHTTPRequestHandler):
             
             self._set_headers(201)
             self.wfile.write(json.dumps(response).encode())
+        else:
+            self._set_headers(404)
+            self.wfile.write(json.dumps({"error": "Invalid endpoint"}).encode())
+    
+    def do_PUT(self):
+        global data_store
+        if self.path.startswith("/items/"):
+            item_id = self.path.split("/")[-1]
+            if item_id.isdigit() and int(item_id) in data_store:
+                content_length = int(self.headers["Content-Length"])
+                put_data = json.loads(self.rfile.read(content_length).decode())
+                
+                data_store[int(item_id)] = put_data
+                self._set_headers()
+                self.wfile.write(json.dumps({"id": int(item_id), "data": put_data}).encode())
+            else:
+                self._set_headers(404)
+                self.wfile.write(json.dumps({"error": "Item not found"}).encode())
         else:
             self._set_headers(404)
             self.wfile.write(json.dumps({"error": "Invalid endpoint"}).encode())
